6625b9c34d86ffe3974a121c5120d5b44b8cc3a0
c0rba <c0rba@DESKTOP-CNVREFN>
2020-09-20 03:59:22 +0200

    SampleIF

---
+++ SampleIF/src/SampleIF/SampleIF.java
@@ -0,0 +1,29 @@
+package SampleIF;
+
+public class SampleIF {
+
+	public static void main(String[] args) {
+		
+		int number=27;
+		
+		if (number == 27) {
+			System.out.println("is true");
+		}
+		
+		else {
+			System.out.println("is false");
+		}
+		
+		int first=1;
+		int second=3;
+		
+		boolean isLesser = first < second;
+		
+		if(isLesser) {
+			System.out.println(first + "is less than " + second + "!");
+			
+		}
+
+	}
+
+}
