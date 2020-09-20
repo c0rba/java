6e30e9e5eca2b671fa801f6e610c5bf7d2403f16
c0rba <c0rba@DESKTOP-CNVREFN>
2020-09-20 18:43:13 +0200

    password

---
+++ Uppgift1-Password/src/Password/Password.java
@@ -0,0 +1,67 @@
+package Password;
+
+import java.util.Scanner;
+
+public class Password {
+
+	public static void main(String[] args) {
+		
+		String password = "Hej123";
+		Scanner scanner= new Scanner(System.in);
+		
+		System.out.println("Enter password here!");
+		
+		while (true) {
+			String pass = scanner.nextLine();
+			
+			if (pass.equals("Quit")) {
+				System.out.println("Get out!");
+				
+				break;
+			}
+			
+			else if (pass.equals(password)) {
+				System.out.println("You entered correct password!");
+				break;
+			}
+			
+			else {
+				System.out.println("You entered wrong password!");
+				
+				Pass(password);
+			}
+		}
+
+	}
+
+	
+	
+	static void Pass(String pass) {
+	 
+		String s = pass;
+		System.out.println("Hint: ");
+		
+		for (int i = 0; i < s.length(); i++) {
+			char c =s.charAt(i);
+			
+			if (c >= 'a' && c <= 'm')
+				c += 10;
+			
+			else if (c >= 'A' && c <='M')
+				c +=10;
+			
+			else if (c >= 'n' && c <='z')
+				c -= 10;
+			
+			else if (c >= 'N' && c <= 'Z')
+				c -= 10;
+			
+			System.out.print(c);
+			
+		}
+		
+		
+		System.out.println();
+		
+	}
+}
